8e024628cd700d010273cddcf424c91bedddc4dd
weiekko <weiekko@gmail.com>
2025-08-11 21:54:26 +0800

    feat: add issue templates, dependabot, workflows, and config files

--- src/config.ts
+++ src/config.ts
@@ -1,86 +1,94 @@
 import type {
-  LicenseConfig,
-  NavBarConfig,
-  ProfileConfig,
-  SiteConfig,
-} from './types/config'
-import { LinkPreset } from './types/config'
+	ExpressiveCodeConfig,
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: '沐印',
-  subtitle: '欢迎光临',
-  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja'
-  themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
-  },
-  banner: {
-    enable: true,
-    src: 'https://img.undf.top/ob/616b27741d92418019d973e0e79add48.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center', // Equivalent to object-position, defaults center
-    credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original zartwork or artist's page
-    }
-  },
-  toc: {
-    enable: true,           // Display the table of contents on the right side of the post
-    depth: 2                // Maximum heading depth to show in the table, from 1 to 3
-  },
-  favicon: [    // Leave this array empty to use the default favicon
-    {
-      src: '/favicon/favicon.ico',    // Path of the favicon, relative to the /public directory
-      theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-      sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    }
-  ]
-}
+	title: "沐印",
+	subtitle: "欢迎光临",
+	lang: "zh_CN", // Language code, e.g. "en", "zh_CN", "ja", etc.
+	themeColor: {
+		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: false, // Hide the theme color picker for visitors
+	},
+	banner: {
+		enable: true,
+		src: "https://img.undf.top/ob/616b27741d92418019d973e0e79add48.png", // Relative to the /src directory. Relative to the /public directory if it starts with "/"
+		position: "center", // Equivalent to object-position, only supports "top", "center", "bottom". "center" by default
+		credit: {
+			enable: false, // Display the credit text of the banner image
+			text: "", // Credit text to be displayed
+			url: "", // (Optional) URL link to the original artwork or artist"s page
+		},
+	},
+	toc: {
+		enable: true, // Display the table of contents on the right side of the post
+		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+	},
+	favicon: [
+		// Leave this array empty to use the default favicon
+		{
+		  src: "/favicon/favicon.ico",    // Path of the favicon, relative to the /public directory
+		  theme: "light",              // (Optional) Either "light" or "dark", set only if you have different favicons for light and dark mode
+		  sizes: "32x32",              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		}
+	],
+};
 
 export const navBarConfig: NavBarConfig = {
-  links: [
-    LinkPreset.Home,
-    LinkPreset.Archive,
-    LinkPreset.About,
-    {
-      name: '友链',
-      url: '/friends/'
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		{
+      name: "友链",
+      url: "/friends/"
     },
     {
-      name: 'GitHub',
-      url: 'https://github.com/wxydejoy',     // Internal links should not include the base path, as it is automatically added
+      name: "GitHub",
+      url: "https://github.com/wxydejoy",     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
-  ],
-}
+	],
+};
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'https://img.undf.top/ob/57a18c5f9a5de165be0e349e90f8eed2.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: '沐印',
-  bio: '欢迎光临',
-  links: [
-    // {
-    //   name: 'Discord',
-    //   icon: 'fa6-brands:discord',       // Visit https://icones.js.org/ for icon codes
-    //                                     // You will need to install the corresponding icon set if it's not already included
-    //                                     // `pnpm add @iconify-json/<icon-set-name>`
-    //   url: 'https://discordapp.com/users/slimenull',
-    // },
-    // {
-    //   name: 'Steam',
-    //   icon: 'fa6-brands:steam',
-    //   url: 'https://steamcommunity.com/id/slimenull/',
-    // },
-    // {
-    //   name: 'GitHub',
-    //   icon: 'fa6-brands:github',
-    //   url: 'https://github.com/SlimeNull',
-    // },
-  ],
-}
+	avatar: "https://img.undf.top/ob/57a18c5f9a5de165be0e349e90f8eed2.png", // Relative to the /src directory. Relative to the /public directory if it starts with "/"
+	name: "沐印",
+	bio: "欢迎光临",
+	links: [
+		// {
+		// 	name: "Twitter",
+		// 	icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+		// 	// You will need to install the corresponding icon set if it"s not already included
+		// 	// `pnpm add @iconify-json/<icon-set-name>`
+		// 	url: "https://twitter.com",
+		// },
+		// {
+		// 	name: "Steam",
+		// 	icon: "fa6-brands:steam",
+		// 	url: "https://store.steampowered.com",
+		// },
+		// {
+		// 	name: "GitHub",
+		// 	icon: "fa6-brands:github",
+		// 	url: "https://github.com/saicaca/fuwari",
+		// },
+	],
+};
 
 export const licenseConfig: LicenseConfig = {
-  enable: true,
-  name: 'CC BY-NC-SA 4.0',
-  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
-}
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+
+export const expressiveCodeConfig: ExpressiveCodeConfig = {
+	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
+	// Please select a dark theme, as this blog theme currently only supports dark background color
+	theme: "github-dark",
+};
